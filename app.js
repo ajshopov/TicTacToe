6fcaf0e9fc484e83947a6d3dc067f244e70ce781
ajshopov <alexanderjshopov@gmail.com>
2017-12-13 12:52:43 +1100

    updated css and draw testing

--- app.js
+++ app.js
@@ -10,30 +10,34 @@ var bottomRight = document.querySelector('.bottom-right');
 var newGame = document.querySelector('.new-game');
 var P1 = document.querySelector('.win-count-P1');
 var P2 = document.querySelector('.win-count-P2');
+var announceBox = document.querySelector('.display-result');
 
 var playerTurn = 'A';
 var gameLogic = [0,0,0,0,0,0,0,0,0];
 var winCounterP1 = 0;
 var winCounterP2 = 0;
+var gameOver = false;
 
 
 function playerMove(event){
-  if(!event.target.classList.contains('cross') && !event.target.classList.contains('nought')){
-    if(playerTurn == 'A'){
-      event.target.classList.add('cross');
-      //console.log(event.target.getAttribute('data-cell'));
-      gameLogic[event.target.getAttribute('data-cell')] = true;
-      console.log(gameLogic);
-      playerTurn = 'B';
-    } else if (playerTurn == 'B'){
-      event.target.classList.add('nought');
-      //console.log(event.target.getAttribute('data-cell'));
-      gameLogic[event.target.getAttribute('data-cell')] = false;
-      console.log(gameLogic);
-      playerTurn = 'A';
+  if(gameOver === false){
+    if(!event.target.classList.contains('cross') && !event.target.classList.contains('nought')){
+      if(playerTurn == 'A'){
+        event.target.classList.add('cross');
+        //console.log(event.target.getAttribute('data-cell'));
+        gameLogic[event.target.getAttribute('data-cell')] = true;
+        //console.log(gameLogic);
+        playerTurn = 'B';
+      } else if (playerTurn == 'B'){
+        event.target.classList.add('nought');
+        //console.log(event.target.getAttribute('data-cell'));
+        gameLogic[event.target.getAttribute('data-cell')] = false;
+        //console.log(gameLogic);
+        playerTurn = 'A';
+      }
+      checkForWin();
+      checkForDraw();
     }
-    checkForWin();
-    checkForDraw();
   }
 }
 
@@ -42,13 +46,17 @@ function check3Cells(cell1,cell2,cell3){
     switch(cell1){
       case true:
         console.log('winner: player 1, crosses');
-        alert('winner: player 1, crosses');
+        announceBox.classList.add('announcement');
+        announceBox.textContent = 'player 1, crosses, wins!';
+        gameOver = true;
         winCounterP1++
         P1.textContent = winCounterP1;
         break;
       case false:
         console.log('winner: player 2, noughts');
-        alert('winner: player 2, noughts');
+        announceBox.classList.add('announcement');
+        announceBox.textContent = 'player 2, noughts, wins!';
+        gameOver = true;
         winCounterP2++
         P2.textContent = winCounterP2;
         break;
@@ -57,12 +65,13 @@ function check3Cells(cell1,cell2,cell3){
 }
 
 function checkForDraw(){
-  var sum = gameLogic.reduce(function(a, b) {
-    return a + b;
-    }, 0);
-  if(sum === 5){
-    console.log('draw');
-    alert('draw');
+  if(gameOver === false){
+    if(!gameLogic.includes(0)){
+      gameOver = true;
+      announceBox.classList.add('announcement');
+      announceBox.textContent = 'DRAW';
+      console.log('draw');
+    }
   }
 }
 
@@ -77,12 +86,16 @@ function checkForWin(){
   check3Cells(gameLogic[2], gameLogic[4], gameLogic[6]);
 }
 
+
 function restart(){
   gameLogic = [0,0,0,0,0,0,0,0,0];
+  gameOver = false;
   var allBoxes = document.querySelectorAll('.grid div')
   for (var i = 0; i < allBoxes.length; i++) {
     allBoxes[i].classList.remove('nought', 'cross');
   }
+  announceBox.classList.remove('announcement');
+  announceBox.textContent = "";
 }
 
 topLeft.addEventListener('click', playerMove)
@@ -95,3 +108,4 @@ bottomLeft.addEventListener('click', playerMove)
 bottomMid.addEventListener('click', playerMove)
 bottomRight.addEventListener('click', playerMove)
 newGame.addEventListener('click', restart)
+announceBox.addEventListener('click', restart)
